5e6df8d939ebed817d659e2578919e1ffbe8fbf0
Vadim <diempi3@gmail.com>
2020-09-06 01:49:16 +0200

    banner

---
+++ client/src/Components/Herobanner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import bannerVideo from '../video/banner.mp4'
+
+export default function Herobanner() {
+    return (
+
+        <div class="hero is-fullheight-with-navbar is-white has-background">
+        <video id="vid" class="hero-background is-transparent" autoPlay muted loop src={bannerVideo}></video>
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h3 class="subtitle">
+                    saving water, the smart way.
+                </h3>
+                <a class="button is-black is-rounded is-outlined" href="#pricing">Select a plan</a>
+            </div>
+        </div>
+    </div>
+
+    )
+}
